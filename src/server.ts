039928e49265fafbbeeaef1614c1f78e4cac0564
Gabriel Sobral <asgoth55@gmail.com>
2021-04-19 22:32:42 -0300

    initial commit - first day

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express();
+
+const PORT  = process.env.PORT || 3333
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
